5bb93199026569c74153ed0dbaa7edf54e198ce6
nehaaaav <neha21vichare@gmail.com>
2021-09-06 01:06:44 +0530

    change

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 var names=new Array();
-names[0]="Yaakov";
-names[1]="John";
-names[2]="Jen";
-names[3]="jason";
-names[4]="paul";
-names[5]="frank";
-names[6]="larry";
-names[7]="paula";
-names[8]="laura";
-names[9]="pixar";
+names[0]="Yamini";
+names[1]="Janhavi";
+names[2]="Jenny";
+names[3]="jason durelo";
+names[4]="Priya";
+names[5]="damini";
+names[6]="Jyoti";
+names[7]="Niharika";
+names[8]="Jamna";
+names[9]="Nirmala";
 
 
 for (var i = 0; i < names.length; i++) {
